bdd0848167387d653feee6426f661ceb69fe9270
lautaropanno <lautaropanno@gmail.com>
2024-01-25 03:10:48 -0300

    Correción de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from "./componentes/NavBar/NavBar";
 import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error from "./componentes/Error/Error";
-import { CartProvider } from "./componentes/context/CartContext.jsx";
+import { CartProvider } from "./componentes/Context/CartContext";
 import Carrito from "./componentes/Carrito/Carrito";
 import Checkout from "./componentes/Checkout/Checkout";
 
